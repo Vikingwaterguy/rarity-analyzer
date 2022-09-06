3fda52cfdc67b0c7d29f65c87f9c1d47b0381a80
Vikingwaterdude <94842337+Vikingwaterdude@users.noreply.github.com>
2022-09-06 18:02:57 +1000

    updatelogos

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -15,33 +15,33 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 					>
 						<div className="flex items-center">
 							<span className="mr-2">
-							<Image src="/minted.png" width="20px" height="20px" />
+							<Image src="/minted.svg" width="20px" height="20px" />
 							</span>
 							Minted
 						</div>
 					</a>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://app.ebisusbay.com/collection/scarecro-fright-society/${id}`}
+						href={`https://app.ebisusbay.com/collection/scarecro-fright-society/${id + 1}`}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
 						<div className="flex items-center">
 							<span className="mr-2">
-								<Image src="/Ebisus.png" width="20px" height="20px" />
+								<Image src="/Ebisus.svg" width="20px" height="20px" />
 							</span>
 							Ebisus
 						</div>
 					</a>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://cronoscan.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id} `}
+						href={`https://cronoscan.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id + 1} `}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
 							<div className="flex items-center">
 							<span className="mr-2">
-								<Image src="/Cro.png" width="20px" height="20px" />
+								<Image src="/Cro.svg" width="20px" height="20px" />
 							</span>
 							cronoscan
 						</div>
