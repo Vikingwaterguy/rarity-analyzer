1d93d44e76af9d4f12b6a6a53f361308239d709f
Vikingwaterdude <94842337+Vikingwaterdude@users.noreply.github.com>
2022-09-06 16:50:27 +1000

    updated logos again, redirected urls

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -35,17 +35,13 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 					</a>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://cronoscan.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN} `}
+						href={`https://cronoscan.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id} `}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
-						<div className="flex items-center">
+							<div className="flex items-center">
 							<span className="mr-2">
-								<Image
-									src="/cro.png"
-									width="20px"
-									height="20px"
-								/>
+								<Image src="/Cro.png" width="20px" height="20px" />
 							</span>
 							cronoscan
 						</div>
