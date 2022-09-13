079fe9713c80982afbed580a95b456a79c5469bc
Vikingwaterguy <113419671+Vikingwaterguy@users.noreply.github.com>
2022-09-13 22:24:43 +1000

    update logos and links

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -9,7 +9,7 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 				<>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://minted.network/collections/cronos/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id + 1}`}
+						href={`https://tofunft.com/collection/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id + 1}`}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
@@ -17,25 +17,12 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 							<span className="mr-2">
 							<Image src="/minted.svg" width="20px" height="20px" />
 							</span>
-							Minted
+							TOFU
 						</div>
 					</a>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://app.ebisusbay.com/collection/scarecro-fright-society/${id + 1}`}
-						target="_blank"
-						rel="noopener noreferrer nofollow"
-					>
-						<div className="flex items-center">
-							<span className="mr-2">
-								<Image src="/Ebisus.svg" width="20px" height="20px" />
-							</span>
-							Ebisus
-						</div>
-					</a>
-					<a
-						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://cronoscan.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id + 1} `}
+						href={`https://bscscan.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id + 1} `}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
@@ -43,7 +30,7 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 							<span className="mr-2">
 								<Image src="/Cro.svg" width="20px" height="20px" />
 							</span>
-							cronoscan
+							BSCSCAN
 						</div>
 					</a>
 				</>
